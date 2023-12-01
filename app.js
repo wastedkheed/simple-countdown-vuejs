cadc82e41c4be31fa0d4604fd7556cf7b5f3a421
wastedkheed <152497298+wastedkheed@users.noreply.github.com>
2023-12-01 17:16:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = new Vue({
    el: "#app",
    data: {
-      title: "Aditya's Birthday in:",
+      title: "Steppa's Birthday in:",
       utcDate: "",
       myDate: "",
       yourDate: "",
